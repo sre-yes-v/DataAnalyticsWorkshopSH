88829054a51c697f3293e3b4a0fc63bc37755d22
sre-yes-v <sreyesv24@gmail.com>
2025-12-29 22:58:30 +0530

    added seo

--- data-analytics-workshop/app/layout.tsx
+++ data-analytics-workshop/app/layout.tsx
@@ -4,8 +4,55 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 export const metadata: Metadata = {
-  title: "Sentiman Coffee Workshop",
-  description: "Learn how to master your brew at Sentiman Coffee workshops in Ubud, Bali",
+  title: "Data Analytics Workshop | Sacred Heart College",
+  description:
+    "Join our Data Analytics Workshop at Sacred Heart College from 22-24 January 2025. Learn data analysis, visualization, and machine learning with expert instructors. Enhance your skills with hands-on sessions and real-world projects. Register now!",
+  viewport: {
+    width: 'device-width',
+    initialScale: 1.0,
+  },
+  openGraph: {
+    title: "Data Analytics Workshop | Sacred Heart College",
+    description:
+      "Join our Data Analytics Workshop at Sacred Heart College from 22-24 January 2025. Learn data analysis, visualization, and machine learning with expert instructors. Enhance your skills with hands-on sessions and real-world projects. Register now!",
+    url: "",
+    siteName: "Data Analytics Workshop",
+    images: [
+      {
+        url: "/CASA_logo.png",
+        width: 1600,
+        height: 730,
+        alt: "Casa | Dept. of CS ",
+        type: "image/jpeg",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Data Analytics Workshop | Sacred Heart College",
+    description:
+      "Join our Data Analytics Workshop at Sacred Heart College from 22-24 January 2025. Learn data analysis, visualization, and machine learning with expert instructors. Enhance your skills with hands-on sessions and real-world projects. Register now!",
+    images: ["/CASA_logo.png"],
+  },
+  alternates: {
+    canonical: "",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  authors: [{ name: "Sreyes V" }],
+  publisher: "Sacred Heart College, Thevara",
 };
 
 export default function RootLayout({
